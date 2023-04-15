9dbb488c9b1c30ad0f1b30d18d756f1144cca9f0
huytran845 <huytrit@gmail.com>
2023-04-15 01:48:45 -0700

    Frame Setup Title Buttons, and added some character animations

---
+++ scenes/title_screen.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _on_continue_butt_pressed():
+	pass # Replace with function body.
+
+
+func _on_credits_butt_pressed():
+	pass # Replace with function body.
+
+
+func _on_new_game_butt_pressed():
+	pass # Replace with function body.
