a8d701c87468cc5555bbd8774095e7f90c5fc913
huytran845 <huytrit@gmail.com>
2023-04-15 10:53:25 -0700

    Adjusted Naming for Titles Screen Buttons
    Added source picture used for title screen

--- scenes/title_screen.gd
+++ scenes/title_screen.gd
@@ -1,17 +1,14 @@
 extends Control
 
-var mainScene
+var mainScene = get_tree().get_root().get_node("Main")
 
-func _ready():
-	mainScene = get_tree().get_root().get_node("Main")
-
-func _on_credits_butt_pressed():
+func _on_new_game_butt_pressed():
 	mainScene.loadLevel1()
 
 
 func _on_continue_butt_pressed():
-	pass # Replace with function body.
+	mainScene.loadLevel1()
 
 
-func _on_new_game_butt_pressed():
+func _on_credits_butt_pressed():
 	pass # Replace with function body.
